0be97d4085c4a45ad7aa9342bd9f6ff87a2fc051
Stanislav Zmiev <szmiev2000@gmail.com>
2021-03-30 09:28:04 -0400

    First draft of full sql

--- db.sql
+++ db.sql
@@ -1,24 +1,100 @@
+CREATE TABLE Location (
+    GooglePlaceId INT PRIMARY KEY NOT NULL,
+    Name          TEXT,
+    Latitude      REAL            NOT NULL,
+    Longitude     REAL            NOT NULL
+);
+
+CREATE TABLE University (
+    UniversityID INT PRIMARY KEY NOT NULL,
+    StudentCount INT             NOT NULL,
+    Name         TEXT            NOT NULL,
+    Description  TEXT,
+    AvatarImage  LO,
+    EmailDomain  TEXT            NOT NULL
+);
+
+CREATE TABLE UniversityLocation (
+    GooglePlaceId INT NOT NULL,
+    UniversityID  INT NOT NULL,
+    FOREIGN KEY (GooglePlaceId) REFERENCES Location 
+                          ON DELETE CASCADE
+                          ON UPDATE CASCADE,
+    FOREIGN KEY (UniversityID) REFERENCES University 
+                          ON DELETE CASCADE
+                          ON UPDATE CASCADE,
+);
+
+CREATE TABLE USER (
+    UserID INT PRIMARY KEY NOT NULL,
+    Email TEXT             NOT NULL,
+    Password TEXT          NOT NULL,
+);
+
+CREATE TABLE SuperAdmin (
+    UserID INT NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES User
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE
+);
+
+CREATE TABLE StudiesAt (
+    UserID       INT NOT NULL,
+    UniversityID INT NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES User 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+    FOREIGN KEY (UniversityID) REFERENCES University 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+);
+
+CREATE TABLE RSO (
+    RSOID       INT PRIMARY KEY NOT NULL,
+    Name        TEXT            NOT NULL,
+    Description TEXT,
+    AdminID     INT             NOT NULL,
+    FOREIGN KEY (AdminID) REFERENCES User
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE
+);
+
+CREATE TABLE RegisteredAt (
+    RSOID           INT NOT NULL,
+    UniversityID    INT NOT NULL,
+    FOREIGN KEY (RSOID) REFERENCES RSO 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+    FOREIGN KEY (UniversityID) REFERENCES University 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+);
+
+CREATE TABLE MemberOf (
+    UserID          INT NOT NULL,
+    RSOID           INT NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES User 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+    FOREIGN KEY (RSOID) REFERENCES RSO 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+);
+
 CREATE TABLE Event (
-   EventID  INT PRIMARY KEY NOT NULL,
-   Summary        TEXT      NOT NULL,
+   EventID        INT PRIMARY KEY NOT NULL,
+   Summary        TEXT            NOT NULL,
+   PrivacyLevel   INT             NOT NULL,
    Description    TEXT,
    Phone          TEXT,
    Email          TEXT,
-   Dtstart        TIMESTAMP NOT NULL,
-   Dtend          TIMESTAMP NOT NULL,
-   Dtstart        TIMESTAMP NOT NULL,
-   Until          TIMESTAMP NOT NULL,
-   Rrule          TEXT,
+   Dtstart        TIMESTAMP       NOT NULL,
+   Dtend          TIMESTAMP       NOT NULL,
+   Until          TIMESTAMP,
+   Rrule          TEXT
 );
 
-CREATE TABLE Location (
-    GooglePlaceId   INT PRIMARY KEY NOT NULL,
-    Name            TEXT,
-    Latitude        REAL            NOT NULL,
-    Longitude       REAL            NOT NULL
-);
-
-CREATE TABLE LocatedAt (
+CREATE TABLE EventLocation (
     GooglePlaceId   INT NOT NULL,
     EventID         INT NOT NULL,
     FOREIGN KEY (GooglePlaceId) REFERENCES Location 
@@ -30,16 +106,41 @@ CREATE TABLE LocatedAt (
 );
 
 CREATE TABLE Comment (
-    EventID       INT       NOT NULL,
-    UserID        INT       NOT NULL,
-    PostTimestamp TIMESTAMP NOT NULL,
-    Text          TEXT      NOT NULL,
-    Rating        INT      NOT NULL,
-    PRIMARY KEY (PostTimestamp),
-    FOREIGN KEY (EventID) REFERENCES Event 
-                          ON DELETE CASCADE
-                          ON UPDATE CASCADE,
+    Postdate  TIMESTAMP PRIMARY KEY NOT NULL,
+    Text      TEXT                  NOT NULL,
+    UserID    INT                   NOT NULL,
+    EventID   INT                   NOT NULL,
     FOREIGN KEY (UserID) REFERENCES User 
-                          ON DELETE CASCADE
-                          ON UPDATE CASCADE,
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+    FOREIGN KEY (EventID) REFERENCES Event 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+);
+
+CREATE TABLE Organizes (
+    RSOID           INT NOT NULL,
+    EventID         INT NOT NULL,
+    FOREIGN KEY (RSOID) REFERENCES RSO 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+    FOREIGN KEY (EventID) REFERENCES Event 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+);
+
+CREATE EventTag (
+    EventTagID   INT PRIMARY KEY NOT NULL,
+    EventTagName TEXT            NOT NULL
+);
+
+CREATE TABLE EventCategory (
+    EventID    INT NOT NULL,
+    EventTagID INT NOT NULL,
+    FOREIGN KEY (EventID) REFERENCES Event 
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
+    FOREIGN KEY (EventTagID) REFERENCES EventTag
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
 );
