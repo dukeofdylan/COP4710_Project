83a8b0037a0784d2966636b5f4b8b06ebaf00e2c
Anthony Garcia Jr <potentshull@gmail.co>
2021-04-13 11:24:51 -0400

    Nav bar now uses flexbox

--- project/static/static_dirs/main/onload.js
+++ project/static/static_dirs/main/onload.js
@@ -19,24 +19,4 @@ function searchDBEntity(input_id, list_id) {
 }
 jQuery(function ($) {
     $(".date-input").datepicker();
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById(input_id);
-  filter = input.value.toUpperCase();
-  ul = document.getElementById(list_id);
-  li = ul.getElementsByTagName('li');
-  
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-      } else {
-      li[i].style.display = "none";
-      }
-  }
-}
-jQuery(function ($) {
-    $(".date-input").datepicker();
 });
